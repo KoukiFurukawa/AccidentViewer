5e6a1af4f382e790d07c4b74c4b604e68a091249
watayuu <ok220145@oks.iput.ac.jp>
2025-01-16 13:22:11 +0900

    App.tsx内部変更

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,60 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react' 
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+const width =1000
+const height =400
+const smallRects = 10
+const smallCircles = 15
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [png, setPng] = useState<string | null>(null)
+
+  useEffect(() => {
+    const canvasElem = document.createElement('canvas')
+    canvasElem.width = width
+    canvasElem.height = height
+    const ctx = canvasElem.getContext('2d')
+
+    // draw
+
+    ctx.clearRect(0, 0, width, height)
+    ctx.fillStyle = '#f7f7f7'
+    ctx.fillRect(0, 0, width, height)
+
+    for (let i = 0; i < smallRects; i++ ) {
+      const rectWidth = 75
+      const rectHeight = 300
+      const x = 150 * i
+      const y = 50
+
+      ctx.fillStyle = '#40c25c'
+      ctx.fillRect(x, y, rectWidth, rectHeight)
+    }
+
+    const radius = 10
+    const peplex = 30
+    const pepley = 30
+
+    ctx.beginPath()
+    ctx.arc(peplex, pepley, radius, 0, Math.PI * 2)
+    ctx.fillStyle = 'red'
+    ctx.fill()
+    ctx.closePath()
+
+    setPng(canvasElem.toDataURL())
+  }, [])
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div>
+      <h1>全体マップ</h1>
+      <h4>生成</h4>
+      {png && (
+        <div className="comp" style={{ display: 'flex' }}>
+          <img alt="icon" src={png} />
+        </div>
+      )}
+    </div>
   )
 }
 
