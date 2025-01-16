5b23cb7dd7b0a8b5af42af0adfa08b28ee62f147
watayuu <ok220145@oks.iput.ac.jp>
2025-01-16 16:11:51 +0900

    jsonファイルの数値を宣言した

---
+++ src/types/coordinate-date.d.ts
@@ -0,0 +1,10 @@
+declare module '*/coordinate-date.json' {
+    interface CoordinateDate {
+        px: number;
+        py: number;
+        lx: number;
+        ly: number;
+    }
+    const value: CoordinateDate;
+    export default value;
+}
